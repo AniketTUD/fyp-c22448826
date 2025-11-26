e36a51dd15a6e5d87997f0b9fff89d0bf02331bf
Aniket Bedade <C22448826@mytudublin.ie>
2025-11-26 21:22:00 +0000

    Change from Maven to Springboot project

--- src/chatbot/src/main/java/com/example/Assistant.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import dev.langchain4j.service.*;
-
-public interface Assistant {
-    @SystemMessage("You are a helpful and friendly chatbot.")
-    String chat(String userMessage);
-}
-
